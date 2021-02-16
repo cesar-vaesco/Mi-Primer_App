5fc872a4165ad44e2be19a2e923ea6715f29de68
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-02-16 10:08:24 -0600

    App: ejemplos de interpolación

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -2,12 +2,17 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-empleado',
-  // templateUrl: './empleado.component.html',
-  template: '<p>Aquí iria un empleado!!!!</p>',
-  // styleUrls: ['./empleado.component.css']
-  styles: ['p {background-color:red}'],
+  templateUrl: './empleado.component.html',
+  // template: '<p>Aquí iria un empleado!!!!</p>',
+  styleUrls: ['./empleado.component.css'],
+  // styles: ['p {background-color:red}'],
 })
 export class EmpleadoComponent implements OnInit {
+  nombre = 'César';
+  apellido = 'Vargas';
+  edad = 3;
+  empresa = 'Vaesco';
+
   constructor() {}
 
   ngOnInit(): void {}
